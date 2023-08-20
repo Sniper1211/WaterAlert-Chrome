5f0562f69067bc84ca2ea912a2a0880c5d6547de
李思柰 <lxx328825321@sina.com>
2023-08-21 00:06:01 +0800

    hello world

--- background.js
+++ background.js
@@ -6,6 +6,15 @@ chrome.runtime.onInstalled.addListener(() => {
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
+    let now = new Date();
+    let end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 21, 0, 0, 0);
+    
+    // 如果现在的时间超过了21点，则取消提醒
+    if (now > end) {
+        chrome.alarms.clear('drinkWaterAlarm');
+        return;
+    }
+
     if (lastAlarmHandled) {
         chrome.action.setBadgeText({text: '喝水'});
         lastAlarmHandled = false;
@@ -15,17 +24,21 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 chrome.action.onClicked.addListener(() => {
     chrome.action.setBadgeText({text: ''});
     lastAlarmHandled = true;
+    createAlarm();  // 当用户点击徽章时，重新创建提醒，确保它始终与用户的最新操作保持同步
 });
 
 function createAlarm() {
+    // 首先，取消之前的提醒
+    chrome.alarms.clear('drinkWaterAlarm');
+
     let now = new Date();
     let start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 8, 0, 0, 0);
     let end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 21, 0, 0, 0);
 
     if (now > start && now < end) {
         chrome.alarms.create('drinkWaterAlarm', {
-            delayInMinutes: 0.5,
-            periodInMinutes: lastAlarmHandled ? 0.5 : 0
+            delayInMinutes: 45,
+            periodInMinutes: lastAlarmHandled ? 45 : 0
         });
     }
 }
